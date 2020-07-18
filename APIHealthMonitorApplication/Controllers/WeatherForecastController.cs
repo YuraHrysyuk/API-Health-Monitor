67fc3df79a2349206f2a52be4d240d58a0571e4b
Yura Hrysyuk <yhrysyuk@gmail.com>
2020-07-18 23:52:29 +0300

    Just test branch work

--- APIHealthMonitorApplication/Controllers/WeatherForecastController.cs
+++ APIHealthMonitorApplication/Controllers/WeatherForecastController.cs
@@ -32,8 +32,7 @@ namespace APIHealthMonitorApplication.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            }).ToArray();
         }
     }
 }

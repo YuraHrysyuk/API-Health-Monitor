2aa2980fa3d2b5db6069195caf09f3fc9a7bafaf
Sergiy Boiko <sergiy.boiko88@gmail.com>
2020-07-26 22:49:23 +0300

    Dynamic navbar v1.0

--- APIHealthMonitorWeb/src/app/home/home.module.ts
+++ APIHealthMonitorWeb/src/app/home/home.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TableComponent } from './table/table.component';
-import { HomeComponent } from './home.component';
-import {MatTableModule} from '@angular/material/table';
-
-
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
-  declarations: [HomeComponent, TableComponent],
+  declarations: [],
   imports: [
     CommonModule,
-    MatTableModule
-  ],
+    SharedModule
+  ]
 })
 export class HomeModule { }

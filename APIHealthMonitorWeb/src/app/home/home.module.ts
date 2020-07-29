8c91e51b58c61893eca646510ca4e9bac6c5d323
Sergiy <48588728+Sergiy-Dev@users.noreply.github.com>
2020-07-29 17:35:17 +0300

    'sw'

--- APIHealthMonitorWeb/src/app/home/home.module.ts
+++ APIHealthMonitorWeb/src/app/home/home.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
 import { HomeComponent } from './home.component';
-import {MatTableModule} from '@angular/material/table';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { GridToolbarComponent } from './grid-toolbar/grid-toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [HomeComponent, TableComponent, SidebarComponent],
+  declarations: [HomeComponent, TableComponent, GridToolbarComponent],
   imports: [
     CommonModule,
     MatTableModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
     MatSidenavModule

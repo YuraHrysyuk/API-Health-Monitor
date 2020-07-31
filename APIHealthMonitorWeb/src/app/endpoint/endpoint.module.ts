c1e5d97ab01c02d3f8d50cca47f56192bfa9560c
Sergiy Boiko <sergiy.boiko88@gmail.com>
2020-08-01 00:15:14 +0300

    Endpoint Expansion panel

--- APIHealthMonitorWeb/src/app/endpoint/endpoint.module.ts
+++ APIHealthMonitorWeb/src/app/endpoint/endpoint.module.ts
@@ -4,8 +4,6 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import { EndpointComponent } from './endpoint.component';
 import { ExpansionPanelComponent } from './expansion-panel/expansion-panel.component';
 
-
-
 @NgModule({
   declarations: [EndpointComponent, ExpansionPanelComponent],
   imports: [

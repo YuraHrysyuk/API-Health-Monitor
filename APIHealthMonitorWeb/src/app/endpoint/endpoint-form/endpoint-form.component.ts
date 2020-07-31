683ab1f674ccb7aba2c75f620862673458891055
Sergiy Boiko <sergiy.boiko88@gmail.com>
2020-08-01 02:21:43 +0300

    Endpoint Sidebar with webform v1

---
+++ APIHealthMonitorWeb/src/app/endpoint/endpoint-form/endpoint-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder, FormGroupDirective } from '@angular/forms';
+
+@Component({
+  selector: 'app-endpoint-form',
+  templateUrl: './endpoint-form.component.html',
+  styleUrls: ['./endpoint-form.component.scss']
+})
+export class EndpointFormComponent implements OnInit {
+  formGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+      connectionPath: [''],
+      comments: ['']
+    });
+  }
+
+  onSubmit(form: any, formDirective: FormGroupDirective) {
+    console.log(this.formGroup.value);
+    formDirective.resetForm();
+    this.formGroup.reset();
+  }
+
+}
+
+
+
+
+
--- APIHealthMonitorWeb/src/app/endpoint/endpoint.module.ts
+++ APIHealthMonitorWeb/src/app/endpoint/endpoint.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 import { EndpointComponent } from './endpoint.component';
 import { ExpansionPanelComponent } from './expansion-panel/expansion-panel.component';
+import { PanelToolbarComponent } from './panel-toolbar/panel-toolbar.component';
+import { EndpointFormComponent } from './endpoint-form/endpoint-form.component';
 
 @NgModule({
-  declarations: [EndpointComponent, ExpansionPanelComponent],
+  declarations: [EndpointComponent, ExpansionPanelComponent, PanelToolbarComponent, EndpointFormComponent],
   imports: [
     CommonModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule
   ]
 })
 export class EndpointModule { }

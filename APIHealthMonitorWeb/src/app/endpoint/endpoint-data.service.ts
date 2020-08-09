3ff2cd418468addd11855a312481ffddd09e421c
Sergiy Boiko <sergiy.boiko88@gmail.com>
2020-08-10 01:02:46 +0300

    Getting endpoints by Scenario ID

--- APIHealthMonitorWeb/src/app/endpoint/endpoint-data.service.ts
+++ APIHealthMonitorWeb/src/app/endpoint/endpoint-data.service.ts
@@ -7,10 +7,14 @@ import { Injectable } from '@angular/core';
 })
 export class EndpointDataService {
   private URL = 'https://localhost:5001/Scenario';
+  private endpointsURL = 'https://localhost:5001/scenarios';
 
   constructor(private http: HttpClient) { }
 
   getScenarios() {
     return this.http.get(this.URL);
+  }
+  getEndPoint(id: number) {
+    return this.http.get(this.endpointsURL + `/${id}/end-points`);
   }
 }

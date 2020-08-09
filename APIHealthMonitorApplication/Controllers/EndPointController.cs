1cd14b930eb15313f85251411cfda79f36e7d8c5
Yura Hrysyuk <yhrysyuk@gmail.com>
2020-08-09 19:47:50 +0300

    Added Endpoint Controller

---
+++ APIHealthMonitorApplication/Controllers/EndPointController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIHealthMonitorApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EndPointController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<EndPoint> Get()
+        {
+            return new List<EndPoint>() {
+                new EndPoint() {
+                    Id = 1,
+                    Name = "First Test Group",
+                    Description = "Some Long Description Here 1",
+                    ConnectionPath = "/"
+                   
+                },
+                new EndPoint() {
+                    Id = 2,
+                    Name = "Second Test Group",
+                    Description = "Some Long Description Here 2",
+                    ConnectionPath = "/"
+                },
+                new EndPoint() {
+                    Id = 3,
+                    Name = "Third Test Group",
+                    Description = "Some Long Description Here 3",
+                    ConnectionPath = "/"
+                },
+                new EndPoint() {
+                    Id = 4,
+                    Name = "Forth Test Group",
+                    Description = "Some Long Description Here 4",
+                    ConnectionPath = "/"
+                },
+                new EndPoint() {
+                    Id = 5,
+                    Name = "Fifth Test Group",
+                    Description = "Some Long Description Here 5",
+                    ConnectionPath = "/"
+                },
+            };
+        }
+    }
+}
--- APIHealthMonitorApplication/EndPoint.cs
+++ APIHealthMonitorApplication/EndPoint.cs
@@ -2,11 +2,13 @@
 
 namespace APIHealthMonitorApplication
 {
-    public class Scenario
+    public class EndPoint
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public string ConnectionPath { get; set; }
 
         public string Description { get; set; }
 
